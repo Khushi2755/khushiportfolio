f69bac846253ae79f7d03d435cc687393d7b70df
Khushii <145796381+Khushi2755@users.noreply.github.com>
2024-10-22 14:43:57 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Khushi Lohan",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  email: "221121@iiitt.ac.in",
+  gitHub: "https://github.com/Khushi2755",
+  linkedIn: "https://www.linkedin.com/in/khushilohan/",
 };
 
 const primaryColor = "#4E567E";
